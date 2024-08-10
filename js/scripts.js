8d751621350beeef8ee2a1d7aad6694f7d5e9535
CATEVA26 <119013401+CATEVA26@users.noreply.github.com>
2024-08-10 15:22:48 -0500

    FIX CSS y JS Preguntas

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
-let templatesCache = {}; 
+let templatesCache = {};
 
+// Menu de pausa
 document.addEventListener("DOMContentLoaded", function() {
     const pauseButton = document.querySelector('.control-icon[alt="Pausa"]');
     const pauseMenu = document.createElement('div');
@@ -15,6 +16,18 @@ document.addEventListener("DOMContentLoaded", function() {
     document.body.appendChild(pauseMenu);
 
     const resumeButton = document.querySelector('.resume-btn');
+
+    pauseButton.addEventListener('click', function () {
+        pauseMenu.style.display = 'flex';
+        document.querySelector('.pantalla-juego').style.filter = 'blur(5px)';
+        document.querySelector('.pantalla-juego').style.pointerEvents = 'none';
+    });
+
+    resumeButton.addEventListener('click', function () {
+        pauseMenu.style.display = 'none';
+        document.querySelector('.pantalla-juego').style.filter = '';
+        document.querySelector('.pantalla-juego').style.pointerEvents = '';
+    });
     const timerElement = document.querySelector('.timer');
 
     let timerDuration = 3 * 60; // 10 minutos en segundos
@@ -61,30 +74,119 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
-function redirigir(pagina) {
-    window.location.href = `/${pagina}.html`;
-}
-
 // Funcion para cambiar de pantalla
-function redirigir(pagina) {
+async function redirigir(pagina) {
+    comprobarPregunta()
     window.location.href = pagina + '.html';
 }
 
+let idPreguntaActual = ""//viene en forma 'pregunta1-Tipo2'
+let contadorRespuestasCorrectas = 0
+let posibleRespuesta1
+let posibleRespuesta2
+let respuestasCorrectas
+function comprobarPregunta() {
+    //Primero buscar la pregunta cargada y buscar el template
+    alert(idPreguntaActual)
+    console.log("comprobandoRespuesta")
+    const plantilla = templatesCache[idPreguntaActual];
+    let preguntaEncontrada
+    if (idPreguntaActual.includes("Tipo2", 0)) {
+        preguntaEncontrada = preguntasTipo2.find(p => p.preguntaID === idPreguntaActual)
+        const verificarRespuestaTipo2 = (preguntaEncontrada) => {
+            let numPregunta = parseInt(String(preguntaEncontrada.preguntaID).charAt(8));
+            compararRespuestasTipo2(numPregunta, preguntaEncontrada)
+        }
+        verificarRespuestaTipo2(preguntaEncontrada)
+        alert(contadorRespuestasCorrectas)
+    } else if (idPreguntaActual.includes("Tipo1", 0)) {
+        preguntaEncontrada = preguntasTipo1.find(p => p.preguntaID === idPreguntaActual)
+        const verificarRespuestaTipo1 = (preguntaEncontrada) => {
+            let numPregunta = parseInt(String(preguntaEncontrada.preguntaID).charAt(8));
+            compararRespuestasTipo1(numPregunta, preguntaEncontrada);
+        }
+        verificarRespuestaTipo1(preguntaEncontrada)
+        alert(contadorRespuestasCorrectas)
+    }
+}
+
+function compararRespuestasTipo2(numPregunta, preguntaEncontrada) {
+    switch (numPregunta) {
+        case 1:
+            break
+        case 2:
+            break
+        case 3:
+            break
+    }
+}
+
+function compararRespuestasTipo1(numPregunta, preguntaEncontrada) {
+    alert("comparando..")
+    switch (numPregunta) {
+        case 1: //PROPANONA
+            posibleRespuesta1 = document.querySelector('.formula-input.op1'); //O
+            posibleRespuesta2 = document.querySelector('.formula-input.op2'); //CH
+            posibleRespuesta1 = posibleRespuesta1.value.toUpperCase();
+            posibleRespuesta2 = posibleRespuesta2.value.toUpperCase();
+            respuestasCorrectas = preguntaEncontrada.respuestas;
+
+            if (posibleRespuesta1 == respuetasCorrectas[0] && posibleRespuesta2 == respuetasCorrectas[1]) {
+                contadorRespuestasCorrectas += contadorRespuestasCorrectas + 100;
+                alert("La respuesta Es correcta");
+            } else {
+                alert("La respuesta Es incorrecta");
+            } //TODO: Brit aca es la parte de las vidas en el else
+
+            break;
+        case 2: //ETANOL
+            posibleRespuesta1 = document.querySelector('.formula-input.op1'); //O
+            posibleRespuesta2 = document.querySelector('.formula-input.op2'); //H
+            posibleRespuesta1 = posibleRespuesta1.value.toUpperCase();
+            posibleRespuesta2 = posibleRespuesta2.value.toUpperCase();
+            respuestasCorrectas = preguntaEncontrada.respuestas;
+
+            if (posibleRespuesta1 == respuestasCorrectas[0] && posibleRespuesta2 == respuestasCorrectas[1]) {
+                contadorRespuestasCorrectas += contadorRespuestasCorrectas + 100;
+                alert("La respuesta Es correcta");
+            } else {
+                alert("La respuesta Es incorrecta");
+            } //TODO: Brit aca es la parte de las vidas en el else
+
+            break;
+        case 3: //PENTANOL
+            posibleRespuesta1 = document.querySelector('.formula-input.op1'); //O
+            posibleRespuesta1 = posibleRespuesta1.value.toUpperCase();
+            respuestasCorrectas = preguntaEncontrada.respuestas;
+
+            if (posibleRespuesta1 == respuestasCorrectas[0]) {
+                contadorRespuestasCorrectas += contadorRespuestasCorrectas + 100;
+                alert("La respuesta Es correcta");
+            } else {
+                alert("La respuesta Es incorrecta");
+            } //TODO: Brit aca es la parte de las vidas en el else
+
+            break;
+    }
+}
 
 // Cargar templates con preguntas
-function cargarTemplates() {
-    fetch('templates.html')
-        .then(response => response.text())
-        .then(data => {
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(data, 'text/html');
+async function cargarTemplates() {
+    try {
+        const randomParam = '?nocache=' + new Date().getTime();
+        const response = await fetch('templates.html' + randomParam);
+        const data = await response.text();
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(data, 'text/html');
 
-            // Cachear los templates
-            doc.querySelectorAll('template').forEach(template => {
-                templatesCache[template.id] = template;
-            });
-        })
-        .catch(error => console.error('Error al cargar los templates:', error));
+        doc.querySelectorAll('template').forEach(template => {
+            templatesCache[template.id] = template;
+        });
+
+        console.log('Templates cargados correctamente');
+    } catch (error) {
+        console.error('Error al cargar los templates:', error);
+    }
 }
 
 
@@ -120,7 +222,7 @@ function aplicarEstilosPregunta(idPregunta) {
 // Función para cambiar el contenido de una pregunta
 function cargarPregunta(idPregunta) {
     const contenedorJuego = document.querySelector('.game-content');
-        
+    idPreguntaActual = idPregunta
     // Asegúrate de que el template existe antes de intentar clonar su contenido
     const plantilla = templatesCache[idPregunta];
     console.log(plantilla);
@@ -139,26 +241,45 @@ function cargarPregunta(idPregunta) {
 // Funcion para cargar las preguntas aleatoreamente
 // Tipo 1
 const preguntasTipo1 = [
-    'pregunta1-Tipo1',
-    'pregunta2-Tipo1',
-    'pregunta3-Tipo1',
+    {
+        preguntaID: 'pregunta1-Tipo1',
+        respuestas: ['O', 'CH']
+    },
+    {
+        preguntaID: 'pregunta2-Tipo1',
+        respuestas: ['O', 'H']
+    },
+    {
+        preguntaID: 'pregunta3-Tipo1',
+        respuestas: ['OH']
+    },
 ];
 function cargarPreguntaAleatoriaTipo1() {
     const indiceAleatorio = Math.floor(Math.random() * preguntasTipo1.length);
-    const idPreguntaAleatoria = preguntasTipo1[indiceAleatorio];
+    const idPreguntaAleatoria = preguntasTipo1[indiceAleatorio].preguntaID;
     cargarPregunta(idPreguntaAleatoria);
 }
 
 
 // Tipo 2
 const preguntasTipo2 = [
-    'pregunta1-Tipo2',
-    'pregunta2-Tipo2',
-    'pregunta3-Tipo2',
+    {
+        preguntaID: 'pregunta1-Tipo2',
+        respuestas: ['Etanol', 'Pentanol']
+    },
+    {
+        preguntaID: 'pregunta2-Tipo2',
+        respuestas: ['Acetona', 'Pentanol']
+    },
+    {
+        preguntaID: 'pregunta3-Tipo2',
+        respuestas: ['Etanol', 'Acetona']
+    },
+
 ];
 function cargarPreguntaAleatoriaTipo2() {
     const indiceAleatorio = Math.floor(Math.random() * preguntasTipo2.length);
-    const idPreguntaAleatoria = preguntasTipo2[indiceAleatorio];
+    const idPreguntaAleatoria = preguntasTipo2[indiceAleatorio].preguntaID;
     cargarPregunta(idPreguntaAleatoria);
 }
 
--- js/sound.js
+++ js/sound.js
@@ -2,4 +2,3 @@ function playSound(soundId) {
     var sound = document.getElementById(soundId);
     sound.play();
 }
-
